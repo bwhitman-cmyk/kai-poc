382d2e634e07260e74b3f8990df1ace9f9c6dbbd
bwhitman-cmyk <bwhitman@whitmanstrategies.com>
2026-02-21 22:12:29 -0500

    Update Sw.js

--- Sw.js
+++ Sw.js
@@ -1,30 +1,28 @@
-const CACHE = "kai-cache-v4";
+const CACHE = "kai-cache-v1";
 const ASSETS = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./Sw.js"
+  "./icon-192-v3.png",
+  "./icon-512-v3.png",
+  "./apple-touch-icon-v3.png"
 ];
-self.addEventListener("install", (e)=>{
-  e.waitUntil(caches.open(CACHE).then(c=> c.addAll(ASSETS)).then(()=> self.skipWaiting()));
-});
 
-self.addEventListener("activate", (e)=>{
+self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => k === CACHE ? null : caches.delete(k))))
-      .then(()=> self.clients.claim())
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener("fetch", (e)=>{
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k === CACHE ? null : caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request).then(net=>{
-      // cache new GET requests (best-effort)
-      if(e.request.method === "GET"){
-        const copy = net.clone();
-        caches.open(CACHE).then(c=> c.put(e.request, copy)).catch(()=>{});
-      }
-      return net;
-    }).catch(()=> res))
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
